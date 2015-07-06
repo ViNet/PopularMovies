9f3c979ae897c2111681bd436a9783c15b7e1c85
Vit <xsxs2ua@gmail.com>
2015-07-06 15:43:21 +0300

    added Application class

---
+++ app/src/main/java/com/example/vit/popularmovies/MovieApplication.java
@@ -0,0 +1,35 @@
+package com.example.vit.popularmovies;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.example.vit.popularmovies.communication.BusProvider;
+import com.example.vit.popularmovies.rest.RestClient;
+import com.squareup.otto.Bus;
+
+/**
+ * Created by Vit on 2015-07-06.
+ */
+public class MovieApplication extends Application {
+
+    public static final String TAG = "PoMo";
+    static final String CLASS = MovieApplication.class.getSimpleName() + ": ";
+
+    private static MovieApplication application;
+    private RestClient client;
+    private Bus bus = BusProvider.getInstance();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, CLASS + " onCreate()");
+        application = this;
+
+        client = new RestClient(bus);
+    }
+
+    public MovieApplication getInstance(){
+        return application;
+    }
+
+}
