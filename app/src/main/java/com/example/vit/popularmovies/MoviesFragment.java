7899fbed20be0e269f8a26470fe721ade3acc48a
Vit <xsxs2ua@gmail.com>
2015-07-07 11:59:36 +0300

    added movies fragment

---
+++ app/src/main/java/com/example/vit/popularmovies/MoviesFragment.java
@@ -0,0 +1,20 @@
+package com.example.vit.popularmovies;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Vit on 2015-07-07.
+ */
+public class MoviesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movies, container, false);
+    }
+}
