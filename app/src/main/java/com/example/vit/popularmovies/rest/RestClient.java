dbf0152327380f155be3e91cd2d7672b5752896f
Vit <xsxs2ua@gmail.com>
2015-07-13 10:34:52 +0300

    added bus communication for RestClient

--- app/src/main/java/com/example/vit/popularmovies/rest/RestClient.java
+++ app/src/main/java/com/example/vit/popularmovies/rest/RestClient.java
@@ -1,20 +1,32 @@
 package com.example.vit.popularmovies.rest;
 
+import android.util.Log;
+
+import com.example.vit.popularmovies.MovieApplication;
+import com.example.vit.popularmovies.communication.Event;
+import com.example.vit.popularmovies.rest.conf.Constants;
+import com.example.vit.popularmovies.rest.model.Page;
 import com.example.vit.popularmovies.rest.service.ApiService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
 
+import retrofit.Callback;
 import retrofit.RestAdapter;
+import retrofit.RetrofitError;
 import retrofit.converter.GsonConverter;
 
 /**
  * Created by Vit on 26.06.2015.
  */
 public class RestClient {
+    static final String CLASS = RestClient.class.getSimpleName() + ": ";
     private static final String BASE_URL = "http://api.themoviedb.org/3";
     private ApiService apiService;
+    private Bus bus;
 
-    public RestClient() {
+    public RestClient(Bus bus) {
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'")
                 .create();
@@ -26,9 +38,28 @@ public class RestClient {
                 .build();
 
         apiService = restAdapter.create(ApiService.class);
+
+        this.bus = bus;
+        this.bus.register(this);
     }
 
     public ApiService getApiService() {
         return apiService;
+    }
+
+    @Subscribe
+    public void onLoadMoviesEvent(Event.LoadMoviesEvent event){
+        apiService.getMovie("popularity.desc", Constants.API_KEY, new Callback<Page>() {
+            @Override
+            public void success(Page page, retrofit.client.Response response) {
+                Log.d(MovieApplication.TAG, CLASS + "success size = " + page.getMovies().size());
+                bus.post(new Event.LoadedMoviesEvent(page.getMovies()));
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d(MovieApplication.TAG, CLASS + "failure error = " + error.getMessage());
+            }
+        });
     }
 }
