c69755f1e23cec55ce81808dfd29da29c3cb4725
Vit <xsxs2ua@gmail.com>
2015-07-13 10:40:43 +0300

    adapted movies fragment for real data representation

--- app/src/main/java/com/example/vit/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/vit/popularmovies/MainActivity.java
@@ -23,40 +23,5 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        RestClient client = new RestClient();
-        client.getApiService().getMovie("popularity.desc", Constants.API_KEY, new Callback<Page>() {
-            @Override
-            public void success(Page page, retrofit.client.Response response) {
-                Log.d(TAG, CLASS + "success size = " + page.getMovies().size());
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Log.d(TAG, CLASS + "failure error = " + error.getMessage());
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/vit/popularmovies/MoviesFragment.java
+++ app/src/main/java/com/example/vit/popularmovies/MoviesFragment.java
@@ -9,6 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.vit.popularmovies.communication.BusProvider;
+import com.example.vit.popularmovies.communication.Event;
+import com.example.vit.popularmovies.rest.model.Movie;
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Vit on 2015-07-07.
  */
@@ -17,7 +26,22 @@ public class MoviesFragment extends Fragment {
     private RecyclerView rvMoviesGrid;
     private RecyclerView.LayoutManager layoutManager;
     private MoviesAdapter adapter;
-    String[] dataset = {"Avatar", "Lord of the Rings", "Interstellar"};
+    private List<Movie> moviesList = new ArrayList<Movie>();
+    private Bus bus;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bus = BusProvider.getInstance();
+        //bus = ((MovieApplication)getActivity().getApplication()).getBus();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        bus.register(this);
+        bus.post(new Event.LoadMoviesEvent());
+    }
 
     @Nullable
     @Override
@@ -30,10 +54,19 @@ public class MoviesFragment extends Fragment {
         rvMoviesGrid.setLayoutManager(layoutManager);
 
         // specify an adapter
-        adapter = new MoviesAdapter(dataset);
+        adapter = new MoviesAdapter(getActivity().getBaseContext(), moviesList);
         rvMoviesGrid.setAdapter(adapter);
-
-
         return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        bus.unregister(this);
+    }
+
+    @Subscribe
+    public void onLoadedMoviesEvent(Event.LoadedMoviesEvent event){
+        adapter.setData(event.getMovieList());
     }
 }
