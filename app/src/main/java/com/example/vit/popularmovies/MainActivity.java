60fba9a28fa7ac16204307a878b38ed136b0de77
Vit <xsxs2ua@gmail.com>
2015-07-15 14:27:04 +0300

    added settings activity

--- app/src/main/java/com/example/vit/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/vit/popularmovies/MainActivity.java
@@ -1,20 +1,14 @@
 package com.example.vit.popularmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.example.vit.popularmovies.rest.RestClient;
-import com.example.vit.popularmovies.rest.conf.Constants;
-import com.example.vit.popularmovies.rest.model.Page;
 
-import retrofit.Callback;
-import retrofit.RetrofitError;
-
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity{
 
     static final String TAG = "PoMo";
     static final String CLASS = MainActivity.class.getSimpleName() + ": ";
@@ -23,5 +17,27 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/vit/popularmovies/SettingsActivity.java
@@ -0,0 +1,35 @@
+package com.example.vit.popularmovies;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pref_with_toolbar);
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, new MyPreferenceFragment()).commit();
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences_fragment);
+        }
+    }
+}
