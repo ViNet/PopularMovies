2c7c33f83f2c239217374e49599bb3bf921a44ab
Vit <xsxs2ua@gmail.com>
2015-07-23 12:53:37 +0300

    added model's for GET /movie/{id}/videos response

---
+++ app/src/main/java/com/example/vit/popularmovies/rest/model/Trailer.java
@@ -0,0 +1,50 @@
+package com.example.vit.popularmovies.rest.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Model for JSON response from themoviedb api
+ * example:
+ * {"id":"552e46439251413f9c000592","iso_639_1":"en","key":"je73b_9JdR0",
+ * "name":"Trailer 2","site":"YouTube","size":720,"type":"Trailer"}
+ */
+public class Trailer {
+
+    private String id;
+    @SerializedName("iso_639_1")
+    private String language;
+    private String key;
+    private String name;
+    private String site;
+    private Integer size;
+    private String type;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getLanguage() {
+        return this.language;
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSite() {
+        return this.site;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}
---
+++ app/src/main/java/com/example/vit/popularmovies/rest/model/TrailersResult.java
@@ -0,0 +1,23 @@
+package com.example.vit.popularmovies.rest.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Model for JSON response from themoviedb api
+ * example:
+ * {"id":87101,"results":[{"id":"552e46439251413f9c000592","iso_639_1":"en","key":"je73b_9JdR0",
+ * "name":"Trailer 2","site":"YouTube","size":720,"type":"Trailer"},{"id":"5480c4d3c3a36829b20041f9"
+ * ,"iso_639_1":"en","key":"62E4FJTwSuc","name":"Official Trailer","site":"YouTube","size":1080,
+ * "type":"Trailer"}]}
+ */
+public class TrailersResult {
+    Integer id;
+    @SerializedName("results")
+    List<Trailer> trailers;
+
+    public List<Trailer> getTrailers() {
+        return this.trailers;
+    }
+}
