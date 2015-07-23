ad49a95be431069730cde539cd168b613b0df571
Vit <xsxs2ua@gmail.com>
2015-07-23 12:59:22 +0300

    added GET /movie/{id}/videos request to APIService

--- app/src/main/java/com/example/vit/popularmovies/rest/service/ApiService.java
+++ app/src/main/java/com/example/vit/popularmovies/rest/service/ApiService.java
@@ -2,27 +2,30 @@ package com.example.vit.popularmovies.rest.service;
 
 import com.example.vit.popularmovies.rest.model.DetailedMovie;
 import com.example.vit.popularmovies.rest.model.Page;
+import com.example.vit.popularmovies.rest.model.TrailersResult;
 
 import retrofit.Callback;
 import retrofit.http.GET;
 import retrofit.http.Path;
 import retrofit.http.Query;
 
-/**
- * Created by Vit on 26.06.2015.
- */
-
 
 public interface ApiService {
 
     // get list of movie
-    // /discover/movie?sort_by=popularity.desc&api_key={api_key}
+    // path - /discover/movie?sort_by=popularity.desc
     @GET("/discover/movie")
     void getMovies(@Query("sort_by") String sortBy, @Query("api_key") String apiKey, Callback<Page> cb);
 
     // get full movie information by it's id
-    // /movie/{id}?api_key={api_key}
+    // path - /movie/{id}
     @GET("/movie/{id}")
     void getDetailedMovie(@Path("id") int id, @Query("api_key") String apiKey, Callback<DetailedMovie> cb);
+
+    // Get the videos (trailers, teasers, clips, etc...) for a specific movie id.
+    // path - /movie/{id}/videos
+    @GET("/movie/{id}/videos")
+    void getVideosByMovieId(@Path("id") int id, @Query("api_key") String apiKey, Callback<TrailersResult> cb);
+
 }
 
